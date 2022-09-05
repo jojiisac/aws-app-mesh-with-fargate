35ea546aefa6b7ed466d192edba89df84ad598a6
Joji <jojiisac@gmail.com>
2022-09-05 11:13:57 +0530

    fargate logging

---
+++ fargatelogging/logging.sh
@@ -0,0 +1,16 @@
+  # create  ns 
+  kubectl apply -f ./namespace.yml
+  # create CM to enable logging 
+  kubectl apply -f ./cm.yml
+  
+
+# craete policy 
+  aws iam create-policy --policy-name eks-fargate-logging-policy --policy-document file://permissions.json
+
+
+  # attachpolicy to pod excecuion role 
+  aws iam attach-role-policy \
+  --policy-arn arn:aws:iam::${ACCOUNT_ID}:policy/eks-fargate-logging-policy \
+  --role-name AmazonEKSFargatePodExecutionRole
+
+kubectl  apply -f log-cerating-app.yml 
